b8f4f643b016b95f9a08060d770beeeaf2633364
Harry Nguyen <70305505+huynxtb@users.noreply.github.com>
2026-01-13 11:16:51 +0700

    feat: create Skip List

---
+++ Data Structures/Skip List/SkipList.py
@@ -0,0 +1,76 @@
+import random
+
+class SkipListNode:
+    def __init__(self, value, level):
+        self.value = value
+        self.forward = [None] * (level + 1)  # Forward references for each level
+
+class SkipList:
+    MAX_LEVEL = 16  # Maximum level of the skip list
+    P = 0.5         # Probability for the random level generator
+
+    def __init__(self):
+        self.level = 0              # Current level of the skip list
+        self.header = SkipListNode(None, self.MAX_LEVEL)  # Header node
+
+    def random_level(self):
+        lvl = 0
+        while random.random() < self.P and lvl < self.MAX_LEVEL:
+            lvl += 1
+        return lvl
+
+    def insert(self, value):
+        update = [None] * (self.MAX_LEVEL + 1)
+        current = self.header
+
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].value < value:
+                current = current.forward[i]
+            update[i] = current
+
+        current = current.forward[0]
+
+        if current is None or current.value != value:
+            new_level = self.random_level()
+            if new_level > self.level:
+                for i in range(self.level + 1, new_level + 1):
+                    update[i] = self.header
+                self.level = new_level
+
+            new_node = SkipListNode(value, new_level)
+            for i in range(new_level + 1):
+                new_node.forward[i] = update[i].forward[i]
+                update[i].forward[i] = new_node
+
+    def delete(self, value):
+        update = [None] * (self.MAX_LEVEL + 1)
+        current = self.header
+
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].value < value:
+                current = current.forward[i]
+            update[i] = current
+
+        current = current.forward[0]
+
+        if current and current.value == value:
+            for i in range(self.level + 1):
+                if update[i].forward[i] != current:
+                    break
+                update[i].forward[i] = current.forward[i]
+
+            while self.level > 0 and self.header.forward[self.level] is None:
+                self.level -= 1
+
+    def search(self, value):
+        current = self.header
+
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].value < value:
+                current = current.forward[i]
+
+        current = current.forward[0]
+
+        if current and current.value == value:
+            return True
+        return False
